54e68e5846cb1f64119841883d1e1483ca10c5df
James Melville <jlmelville@gmail.com>
2019-05-04 20:32:04 -0700

    Remove unused code

--- passaas/app.py
+++ passaas/app.py
@@ -9,7 +9,6 @@ def create_app(config_object=ProdConfig):
 
     app = connexion.App(__name__, specification_dir=config_object.APP_DIR)
     app.app.config.root_path = app.app.instance_path
-    # app.app.json_encoder = encoder.JSONEncoder
 
     # Configuration from config.py
     app.app.config.from_object(config_object)
@@ -23,25 +22,4 @@ def create_app(config_object=ProdConfig):
     # strict_validation means that unexpected query parameters will return 400
     app.add_api("swagger.yml", strict_validation=True)
 
-    register_extensions(app)
-    register_errorhandlers(app)
-    register_shellcontext(app)
-    register_commands(app)
-
     return app
-
-
-def register_extensions(app):
-    pass
-
-
-def register_errorhandlers(app):
-    pass
-
-
-def register_shellcontext(app):
-    pass
-
-
-def register_commands(app):
-    pass
