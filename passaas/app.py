ba5d5460439ba9ed9fa165a600ee9417b2b3553b
James Melville <jlmelville@gmail.com>
2019-05-10 00:26:14 -0700

    Clearer naming for connexion and flask app s

--- passaas/app.py
+++ passaas/app.py
@@ -13,19 +13,20 @@ def create_app(config_object=ProdConfig):
     """
     logging.basicConfig(level=logging.DEBUG)
 
-    app = connexion.App(__name__, specification_dir=config_object.APP_DIR)
-    app.app.config.root_path = app.app.instance_path
+    conn_app = connexion.App(__name__, specification_dir=config_object.APP_DIR)
+    flask_app = conn_app.app
+    flask_app.config.root_path = flask_app.instance_path
 
     # Configuration from config.py
-    app.app.config.from_object(config_object)
+    flask_app.config.from_object(config_object)
 
     # Ignore any configuration override in test configuration
-    if app.app.config["ENV"] != "test":
+    if flask_app.config["ENV"] != "test":
         # Allow configuration override from the instance folder
         # http://flask.pocoo.org/docs/1.0/config/#instance-folders
-        app.app.config.from_pyfile("config.cfg", silent=True)
+        flask_app.config.from_pyfile("config.cfg", silent=True)
 
     # strict_validation means that unexpected query parameters will return 400
-    app.add_api("openapi.yaml", strict_validation=True)
+    conn_app.add_api("openapi.yaml", strict_validation=True)
 
-    return app
+    return conn_app
